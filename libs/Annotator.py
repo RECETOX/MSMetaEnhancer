14ecbe6bda7f0a989b1d082b5fc3eeb790b51b32
Matej Trojak <trojak@mail.muni.cz>
2021-06-08 17:14:07 +0200

    Added Annotator, which will implemented variety of strategies to obtain addtional metadata

---
+++ libs/Annotator.py
@@ -0,0 +1,59 @@
+from libs import converter
+
+
+class Annotator:
+    def __init__(self, annotations):
+        self.annotations = annotations
+
+    def add_possible_annotations(self, metadata):
+        """
+        Adds additional annotations to given dictionary containing metadata
+
+        The method goes through specified annotations and tries to invoke
+        a particular method able to add required annotation.
+
+        :param metadata: given spectra metadata
+        :return: annotated dictionary
+        """
+        for annotation in self.annotations:
+            if annotation not in metadata:
+                try:
+                    result = getattr(self, "add_" + annotation)(metadata)
+                    if result:
+                        metadata[annotation] = result
+                        print(result)
+                except AttributeError:
+                    pass
+        return metadata
+
+    def add_inchikey(self, metadata):
+        """
+        Tries to find an InChiKey based on specified metadata.
+
+        Currently implemented strategies:
+        - CTS service based on CAS number
+
+        :param metadata: specified metadata dictionary
+        :return: found InChiKey (return None if not found)
+        """
+        cas_number = metadata.get('casno', None)
+        if cas_number:
+            if "-" not in cas_number:
+                cas_number = converter.fix_cas_number(cas_number)
+            return converter.cas_to_inchikey(cas_number)
+
+    def add_smiles(self, metadata):
+        """
+        Tries to find an SMILES based on specified metadata.
+
+        Currently implemented strategies:
+        - CIR service based on CAS number
+
+        :param metadata: specified metadata dictionary
+        :return: found SMILES (return None if not found)
+        """
+        cas_number = metadata.get('casno', None)
+        if cas_number:
+            if "-" not in cas_number:
+                cas_number = converter.fix_cas_number(cas_number)
+            return converter.cas_to_smiles(cas_number)
--- libs/MSP.py
+++ libs/MSP.py
@@ -1,13 +1,39 @@
 from matchms.importing import load_from_msp
 from matchms.exporting import save_as_msp
 
+from libs.Annotator import Annotator
+
 
 class MSP:
     def __init__(self):
         self.spectrums = []
 
     def load_msp_file(self, filename):
+        """
+        Loads given MSP filename as a list of matchms.Spectra objects and
+        stores them in self.spectrums attribute
+
+        :param filename: given MSP file
+        """
         self.spectrums = list(load_from_msp(filename))
 
     def save_msp_file(self, filename):
+        """
+        Exports all matchms.Spectra objects stored in self.spectrums to
+        a file given by filename
+
+        :param filename: target MSP file
+        """
         save_as_msp(self.spectrums, filename)
+
+    def annotate_spectrums(self, required_annotations):
+        """
+        Adds additional metadata to all Spectra objects.
+        Required metadata are specified in required_annotations attribute and have to
+        match format of Annotator object.
+
+        :param required_annotations: target annotations
+        """
+        annotator = Annotator(required_annotations)
+        for spectrum in self.spectrums:
+            spectrum.metadata = annotator.add_possible_annotations(spectrum.metadata)
