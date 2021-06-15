477efb6e7db2e6ace3fdd98ad9d4df6b7e8a409f
Matej Trojak <trojak@mail.muni.cz>
2021-06-15 15:36:08 +0200

    Implemented POST requests for general Converter class

--- libs/services/Converter.py
+++ libs/services/Converter.py
@@ -7,22 +7,37 @@ class Converter:
         # the same query multiple times in single session
         self.cache = dict()
 
-    def connect_to_service(self, service, args):
+    def connect_to_service(self, service, args, method='GET'):
         """
         Make get request to given service with arguments.
         Raises ConnectionError if service is not available.
 
         :param service: requested service to be queried
         :param args: additional query arguments
+        :param method: GET (default) or POST
         :return: obtained response
         """
         try:
-            identification = service + ":" + args
+            identification = service + ":" + str(args)
             cached_result = self.cache.get(identification, None)
             if cached_result:
                 return cached_result
-            result = requests.get(self.services[service] + args)
+            result = self.execute_request(self.services[service], args, method)
             self.cache[identification] = result
             return result
         except requests.exceptions.ConnectionError:
             raise ConnectionError('Service {} is not available'.format(service))
+
+    def execute_request(self, url, args, method):
+        """
+        Execute request with type depending on specified method.
+
+        :param url: service URL
+        :param args: given args - either url args for GET or dict for POST
+        :param method: GET/POST
+        :return: obtained response
+        """
+        if method == 'GET':
+            return requests.get(url + args)
+        else:
+            return requests.post(url, data=args)
