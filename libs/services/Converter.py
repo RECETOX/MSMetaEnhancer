d06cfaef0547e6204237c197eb5d4630b28cf9d8
xtrojak <trojak@mail.muni.cz>
2021-06-28 09:20:35 +0200

    Improved code structure in Converter

--- libs/services/Converter.py
+++ libs/services/Converter.py
@@ -16,12 +16,12 @@ class Converter:
         :return: obtained response
         """
         try:
-            result = await self.execute_request(self.services[service] + args, method, data, session)
+            result = await self.loop_request(self.services[service] + args, method, data, session)
             return result
         except TypeError:
             pass  # TODO: log - probably given argument is incorrect
 
-    async def execute_request(self, url, method, data, session, depth=10):
+    async def loop_request(self, url, method, data, session, depth=10):
         """
         Execute request with type depending on specified method.
 
@@ -35,23 +35,34 @@ class Converter:
         try:
             if method == 'GET':
                 async with session.get(url=url) as response:
-                    result = await response.text()
-                    if response.ok:
-                        return result
-                    elif response.status == 503:
-                        if depth > 0:
-                            return await self.execute_request(url, method, data, session, depth - 1)
+                    return await self.process_request(response, url, method, data, session, depth)
             else:
                 async with session.post(url=url, data=data) as response:
-                    result = await response.text()
-                    if response.ok:
-                        return result
-                    elif response.status == 503:
-                        if depth > 0:
-                            return await self.execute_request(url, method, data, session, depth - 1)
+                    return await self.process_request(response, url, method, data, session, depth)
         except ServerDisconnectedError:
             if depth > 0:
-                return await self.execute_request(url, method, data, session, depth - 1)
+                return await self.loop_request(url, method, data, session, depth - 1)
+
+    async def process_request(self, response, url, method, data, session, depth):
+        """
+        Method to wrap response handling (same for POST and GET requests).
+
+        :param response: given async response
+        :param url: service URL
+        :param method: GET/POST
+        :param data: given arguments for POST request
+        :param session: current aiohttp session
+        :param depth: allowed recursion depth for unsuccessful requests
+        :return: processed response
+        """
+        result = await response.text()
+        if response.ok:
+            return result
+        elif response.status == 503:
+            if depth > 0:
+                return await self.loop_request(url, method, data, session, depth - 1)
+        else:
+            pass  # TODO: log - other error responses
 
     async def convert(self, source, target, data, session):
         """
