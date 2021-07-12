d38a1905a5d5a643596fa314b6e88a7a05158fb0
xtrojak <trojak@mail.muni.cz>
2021-07-12 15:07:32 +0200

    Increased code readability and robustness

--- libs/utils/Job.py
+++ libs/utils/Job.py
@@ -1,3 +1,6 @@
+from libs.utils.Errors import ConversionNotSupported, DataNotAvailable
+
+
 class Job:
     def __init__(self, data):
         self.source, self.target, self.service = data
@@ -7,6 +10,17 @@ class Job:
 
     def __repr__(self):
         return f'Job(({self.source}, {self.target}, {self.service}))'
+
+    def validate(self, services, metadata):
+        service = services.get(self.service, None)
+        data = metadata.get(self.source, None)
+
+        if service is None:
+            raise ConversionNotSupported(f'Specified {service} not supported.')
+        elif data is None:
+            raise DataNotAvailable(f'Source {self.source} not available in metadata.')
+        else:
+            return service, data
 
 
 def convert_to_jobs(jobs):
