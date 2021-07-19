41242840164bf10257fc5926b431031023834f00
xtrojak <trojak@mail.muni.cz>
2021-07-19 14:43:19 +0200

    Parametrised tests for CTS using pytest

--- tests/test_CTS.py
+++ tests/test_CTS.py
@@ -1,79 +1,43 @@
 import asyncio
 import unittest
 import json
+import pytest
 
 from libs.services.CTS import CTS
 from tests.utils import wrap_with_session
 
 
-class TestCTS(unittest.TestCase):
-    def setUp(self):
-        self.converter = CTS
+@pytest.mark.parametrize('arg, value, expected, method', [
+    ['inchikey', '7783-89-3', 'XQLMNMQWVCXIKR-UHFFFAOYSA-M', 'cas_to_inchikey'],
+    ['inchi', 'XQLMNMQWVCXIKR-UHFFFAOYSA-M', 'InChI=1S/Ag.BrHO3/c;2-1(3)4/h;(H,2,3,4)/q+1;/p-1', 'inchikey_to_inchi'],
+    ['inchikey', 'L-Alanine', 'QNAYBMKLOCPYGJ-REOHCLBHSA-N', 'name_to_inchikey'],
+    ['name', 'QNAYBMKLOCPYGJ-REOHCLBHSA-N', 'L-2-Aminopropanoic acid', 'inchikey_to_name'],
+    ['iupac_name', 'QNAYBMKLOCPYGJ-REOHCLBHSA-N', '(2S)-2-aminopropanoic acid', 'inchikey_to_iupac_name']
+])
+def test_correct_behavior(arg, value, expected, method):
+    assert asyncio.run(wrap_with_session(CTS, method, [value]))[arg] == expected
 
-    def test_connect_to_service(self):
-        # test basic CTS service
-        cas_number = '7783-89-3'
-        args = 'CAS/InChIKey/{}'.format(cas_number)
-        response = asyncio.run(wrap_with_session(self.converter, 'query_the_service', ['CTS', args]))
-        response_json = json.loads(response)
-        self.assertTrue(type(response_json == list))
-        self.assertEqual(len(response_json), 1)
-        self.assertIn('results', response_json[0])
-        self.assertEqual(len(response_json[0]['results']), 1)
 
-        # incorrect CAS number
-        cas_number = '7783893'
-        args = 'CAS/InChIKey/{}'.format(cas_number)
-        response = asyncio.run(wrap_with_session(self.converter, 'query_the_service', ['CTS', args]))
-        response_json = json.loads(response)
-        self.assertTrue(type(response_json == list))
-        self.assertEqual(len(response_json), 1)
-        self.assertIn('results', response_json[0])
-        self.assertEqual(len(response_json[0]['results']), 0)
+@pytest.mark.parametrize('arg, value, method', [
+    ['inchikey', '7783893', 'cas_to_inchikey'],
+    ['inchi', 'XQLMNMQIKR-UHFFFAOYSA-M', 'inchikey_to_inchi'],
+    ['inchikey', 'L-Alalalalanine', 'name_to_inchikey'],
+    ['name', 'XQLMNMQIKR-UHFFFAOYSA-M', 'inchikey_to_name'],
+    ['iupac_name', 'XQLMNMQIKR-UHFFFAOYSA-M', 'inchikey_to_iupac_name']
+])
+def test_incorrect_behavior(arg, value, method):
+    assert asyncio.run(wrap_with_session(CTS, method, [value])) is None
 
-    def test_cas_to_inchikey(self):
-        inchikey = 'XQLMNMQWVCXIKR-UHFFFAOYSA-M'
-        cas_number = '7783-89-3'
-        self.assertEqual(asyncio.run(wrap_with_session(self.converter, 'cas_to_inchikey', [cas_number]))['inchikey'],
-                         inchikey)
 
-        cas_number = '7783893'
-        self.assertIsNone(asyncio.run(wrap_with_session(self.converter, 'cas_to_inchikey', [cas_number])))
-
-    def test_inchikey_to_inchi(self):
-        inchikey = 'XQLMNMQWVCXIKR-UHFFFAOYSA-M'
-        inchi = 'InChI=1S/Ag.BrHO3/c;2-1(3)4/h;(H,2,3,4)/q+1;/p-1'
-
-        self.assertEqual(asyncio.run(wrap_with_session(self.converter, 'inchikey_to_inchi', [inchikey]))['inchi'],
-                         inchi)
-
-        inchikey = 'XQLMNMQIKR-UHFFFAOYSA-M'
-        self.assertIsNone(asyncio.run(wrap_with_session(self.converter, 'inchikey_to_inchi', [inchikey])))
-
-    def test_name_to_inchikey(self):
-        name = 'L-Alanine'
-        inchikey = 'QNAYBMKLOCPYGJ-REOHCLBHSA-N'
-        self.assertEqual(asyncio.run(wrap_with_session(self.converter, 'name_to_inchikey', [name]))['inchikey'],
-                         inchikey)
-
-        name = 'L-Alalalalanine'
-        self.assertIsNone(asyncio.run(wrap_with_session(self.converter, 'name_to_inchikey', [name])))
-
-    def test_inchikey_to_name(self):
-        inchikey = 'QNAYBMKLOCPYGJ-REOHCLBHSA-N'
-        name = 'L-2-Aminopropanoic acid'
-
-        self.assertEqual(asyncio.run(wrap_with_session(self.converter, 'inchikey_to_name', [inchikey]))['name'], name)
-
-        inchikey = 'XQLMNMQIKR-UHFFFAOYSA-M'
-        self.assertIsNone(asyncio.run(wrap_with_session(self.converter, 'inchikey_to_name', [inchikey])))
-
-    def test_inchikey_to_IUPAC_name(self):
-        inchikey = 'QNAYBMKLOCPYGJ-REOHCLBHSA-N'
-        iupac_name = '(2S)-2-aminopropanoic acid'
-        asyncio.run(wrap_with_session(self.converter, 'inchikey_to_iupac_name', [inchikey]))
-        self.assertEqual(asyncio.run(wrap_with_session(self.converter, 'inchikey_to_iupac_name',
-                                                       [inchikey]))['iupac_name'], iupac_name)
-
-        inchikey = 'XQLMNMQIKR-UHFFFAOYSA-M'
-        self.assertIsNone(asyncio.run(wrap_with_session(self.converter, 'inchikey_to_iupac_name', [inchikey])))
+@pytest.mark.parametrize('value, size', [
+    ['7783-89-3', 1],
+    ['7783893', 0]
+])
+def test_format(value, size):
+    args = 'CAS/InChIKey/{}'.format(value)
+    response = asyncio.run(wrap_with_session(CTS, 'query_the_service', ['CTS', args]))
+    response_json = json.loads(response)
+    assert type(response_json) == list
+    assert len(response_json) == 1
+    assert 'results' in response_json[0]
+    assert len(response_json[0]['results']) == size
