9fb0391d928ef918b54bc3cec603464dd8a6fcaf
xtrojak <trojak@mail.muni.cz>
2021-07-19 14:34:04 +0200

    Parametrised tests for CIR using pytest

--- tests/test_CIR.py
+++ tests/test_CIR.py
@@ -1,81 +1,29 @@
 import asyncio
-import unittest
+import pytest
 
 from libs.services.CIR import CIR
 from tests.utils import wrap_with_session
 
 
-class TestCIR(unittest.TestCase):
-    def setUp(self):
-        self.converter = CIR
+@pytest.mark.parametrize('arg, value, expected, method', [
+    ['smiles', '7783-89-3', '[Ag+].[O-][Br](=O)=O', 'cas_to_smiles'],
+    ['smiles', 'XQLMNMQWVCXIKR-UHFFFAOYSA-M', '[Ag+].[O-][Br](=O)=O', 'inchikey_to_smiles'],
+    ['inchi', 'XQLMNMQWVCXIKR-UHFFFAOYSA-M', 'InChI=1S/Ag.BrHO3/c;2-1(3)4/h;(H,2,3,4)/q+1;/p-1', 'inchikey_to_inchi'],
+    ['casno', 'XQLMNMQWVCXIKR-UHFFFAOYSA-M', '7783-89-3', 'inchikey_to_cas'],
+    ['formula', 'XQLMNMQWVCXIKR-UHFFFAOYSA-M', 'AgBrO3', 'inchikey_to_formula'],
+    ['inchikey', '[Ag+].[O-][Br](=O)=O', 'XQLMNMQWVCXIKR-UHFFFAOYSA-M', 'smiles_to_inchikey']
+])
+def test_correct_behavior(arg, value, expected, method):
+    assert asyncio.run(wrap_with_session(CIR, method, [value]))[arg] == expected
 
-    def test_connect_to_service(self):
-        # test basic CIR service
-        cas_number = '7783-89-3'
-        args = '{}/smiles?resolver=cas_number'.format(cas_number)
-        response = asyncio.run(wrap_with_session(self.converter, 'query_the_service', ['CIR', args]))
-        self.assertTrue(type(response) == str)
 
-        # incorrect CAS number
-        cas_number = '7783893'
-        args = '{}/smiles?resolver=cas_number'.format(cas_number)
-        response = asyncio.run(wrap_with_session(self.converter, 'query_the_service', ['CIR', args]))
-        self.assertIsNone(response)
-
-    def test_cas_to_smiles(self):
-        smiles = '[Ag+].[O-][Br](=O)=O'
-        cas_number = '7783-89-3'
-        self.assertEqual(asyncio.run(wrap_with_session(self.converter, 'cas_to_smiles', [cas_number]))['smiles'],
-                         smiles)
-
-        cas_number = '7783893'
-        self.assertIsNone(asyncio.run(wrap_with_session(self.converter, 'cas_to_smiles', [cas_number])))
-
-    def test_inchikey_to_smiles(self):
-        inchikey = 'XQLMNMQWVCXIKR-UHFFFAOYSA-M'
-        smiles = '[Ag+].[O-][Br](=O)=O'
-        self.assertEqual(asyncio.run(wrap_with_session(self.converter, 'inchikey_to_smiles', [inchikey]))['smiles'],
-                         smiles)
-
-        inchikey = 'XQLMNVCXIKR-UHFFFAOYSA-M'
-        self.assertIsNone(asyncio.run(wrap_with_session(self.converter, 'inchikey_to_smiles', [inchikey])))
-
-    def test_inchikey_to_inchi(self):
-        inchikey = 'XQLMNMQWVCXIKR-UHFFFAOYSA-M'
-        inchi = 'InChI=1S/Ag.BrHO3/c;2-1(3)4/h;(H,2,3,4)/q+1;/p-1'
-
-        self.assertEqual(asyncio.run(wrap_with_session(self.converter, 'inchikey_to_inchi', [inchikey]))['inchi'],
-                         inchi)
-
-        inchikey = 'XQLMNVCXIKR-UHFFFAOYSA-M'
-        self.assertIsNone(asyncio.run(wrap_with_session(self.converter, 'inchikey_to_inchi', [inchikey])))
-
-    def test_inchikey_to_cas(self):
-        inchikey = 'XQLMNMQWVCXIKR-UHFFFAOYSA-M'
-        cas_number = '7783-89-3'
-
-        self.assertEqual(asyncio.run(wrap_with_session(self.converter, 'inchikey_to_cas', [inchikey]))['casno'],
-                         cas_number)
-
-        inchikey = 'XQLMNVCXIKR-UHFFFAOYSA-M'
-        self.assertIsNone(asyncio.run(wrap_with_session(self.converter, 'inchikey_to_cas', [inchikey])))
-
-    def test_inchikey_to_formula(self):
-        inchikey = 'XQLMNMQWVCXIKR-UHFFFAOYSA-M'
-        formula = 'AgBrO3'
-
-        self.assertEqual(asyncio.run(wrap_with_session(self.converter, 'inchikey_to_formula', [inchikey]))['formula'],
-                         formula)
-
-        inchikey = 'XQLMNVCXIKR-UHFFFAOYSA-M'
-        self.assertIsNone(asyncio.run(wrap_with_session(self.converter, 'inchikey_to_formula', [inchikey])))
-
-    def test_smiles_to_inchikey(self):
-        inchikey = 'XQLMNMQWVCXIKR-UHFFFAOYSA-M'
-        smiles = '[Ag+].[O-][Br](=O)=O'
-
-        self.assertEqual(asyncio.run(wrap_with_session(self.converter, 'smiles_to_inchikey', [smiles]))['inchikey'],
-                         inchikey)
-
-        smiles = '[Ag+].O-][Br](=O)=O'
-        self.assertIsNone(asyncio.run(wrap_with_session(self.converter, 'smiles_to_inchikey', [smiles])))
+@pytest.mark.parametrize('arg, value, method', [
+    ['smiles', '7783893', 'cas_to_smiles'],
+    ['smiles', 'XQLMNVCXIKR-UHFFFAOYSA-M', 'inchikey_to_smiles'],
+    ['inchi', 'XQLMNVCXIKR-UHFFFAOYSA-M', 'inchikey_to_inchi'],
+    ['casno', 'XQLMNVCXIKR-UHFFFAOYSA-M', 'inchikey_to_cas'],
+    ['formula', 'XQLMNVCXIKR-UHFFFAOYSA-M', 'inchikey_to_formula'],
+    ['inchikey', '[Ag+].O-][Br](=O)=O', 'smiles_to_inchikey']
+])
+def test_incorrect_behavior(arg, value, method):
+    assert asyncio.run(wrap_with_session(CIR, method, [value])) is None
