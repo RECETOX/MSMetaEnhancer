9326df9218b785f28ac2dc0245c6a2b68b0dac43
xtrojak <trojak@mail.muni.cz>
2021-07-28 13:44:16 +0200

    Changed PubChem service to IDSM (Elixir) using SPAQRL

--- libs/services/PubChem.py
+++ libs/services/PubChem.py
@@ -1,5 +1,3 @@
-import json
-
 from libs.services.Converter import Converter
 from frozendict import frozendict
 
@@ -8,49 +6,108 @@ class PubChem(Converter):
     def __init__(self, session):
         super().__init__(session)
         # service URLs
-        self.services = {'PubChem': 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/'}
+        self.services = {'PubChem': 'https://idsm.elixir-czech.cz/sparql/endpoint/idsm'}
+        self.header = frozendict({"Accept": "application/sparql-results+json"})
 
-        self.attributes = [{'code': 'inchi', 'label': 'InChI', 'extra': None},
-                           {'code': 'inchikey', 'label': 'InChIKey', 'extra': None},
-                           {'code': 'iupac_name', 'label': 'IUPAC Name', 'extra': 'Preferred'},
-                           {'code': 'formula', 'label': 'Molecular Formula', 'extra': None},
-                           {'code': 'smiles', 'label': 'SMILES', 'extra': 'Canonical'}]
+        self.attributes = [{'code': 'inchi', 'label': 'CHEMINF_000396'},
+                           {'code': 'iupac_name', 'label': 'CHEMINF_000382'},
+                           {'code': 'inchikey', 'label': 'CHEMINF_000399'},
+                           {'code': 'formula', 'label': 'CHEMINF_000335'},
+                           {'code': 'smiles', 'label': 'CHEMINF_000376'}]
 
         # generate top level methods defining allowed conversions
         conversions = [('name', 'inchi', 'from_name'),
-                       ('name', 'inchikey', 'from_name'),
                        ('name', 'iupac_name', 'from_name'),
                        ('name', 'formula', 'from_name'),
                        ('name', 'smiles', 'from_name'),
-                       ('inchi', 'inchikey', 'from_inchi'),
                        ('inchi', 'iupac_name', 'from_inchi'),
                        ('inchi', 'formula', 'from_inchi'),
                        ('inchi', 'smiles', 'from_inchi')]
         self.create_top_level_conversion_methods(conversions)
 
-    async def from_name(self, name):
+    async def name_to_inchikey(self, name):
         """
-        Convert Chemical name to all possible attributes using PubChem service
-        More info: https://pubchemdocs.ncbi.nlm.nih.gov/pug-rest
+        Convert Chemical name to InChIKey using IDSM service
 
         :param name: given Chemical name
         :return: all found data
         """
-        args = f'name/{name}/JSON'
-        response = await self.query_the_service('PubChem', args)
-        if response:
-            return self.parse_attributes(response)
-
-    async def from_inchi(self, inchi):
+        query = f"""
+        SELECT DISTINCT ?type ?value
+        WHERE
+        {{
+          ?inchikey sio:has-value ?value.
+          ?inchikey rdf:type ?type.
+          ?inchikey sio:is-attribute-of ?compound.
+          ?synonym sio:is-attribute-of ?compound.
+          ?synonym sio:has-value "{name.lower()}"@en.
+        }}
         """
-        Convert InChi to to all possible attributes using PubChem service
-        More info: https://pubchemdocs.ncbi.nlm.nih.gov/pug-rest
+        return await self.call_service(query)
+
+    async def inchi_to_inchikey(self, inchi):
+        """
+        Convert InChi to InChIKey using IDSM service
 
         :param inchi: given InChi
         :return: all found data
         """
-        args = "inchi/JSON"
-        response = await self.query_the_service('PubChem', args, method='POST', data=frozendict({'inchi': inchi}))
+        query = f"""
+        SELECT DISTINCT ?type ?value
+        WHERE
+        {{
+          ?inchikey sio:has-value ?value.
+          ?inchikey rdf:type ?type.
+          ?inchikey sio:is-attribute-of ?compound.
+          ?compound sio:has-attribute ?inchi.
+          ?inchi sio:has-value "{inchi}"@en.
+        }}
+        """
+        return await self.call_service(query)
+
+    async def from_name(self, name):
+        """
+        Convert Chemical name to all possible attributes using IDSM service
+
+        :param name: given Chemical name
+        :return: all found data
+        """
+        query = f"""
+        SELECT DISTINCT ?value ?type
+        WHERE
+        {{
+          ?attribute rdf:type ?type.
+          ?attribute sio:has-value ?value.
+          ?compound sio:has-attribute ?attribute.
+          ?synonym sio:is-attribute-of ?compound.
+          ?synonym sio:has-value "{name.lower()}"@en.
+        }}
+        """
+        return await self.call_service(query)
+
+    async def from_inchi(self, inchi):
+        """
+        Convert InChi to to all possible attributes using IDSM service
+
+        :param inchi: given InChi
+        :return: all found data
+        """
+        query = f"""
+        SELECT DISTINCT ?value ?type
+        WHERE
+        {{
+          ?attribute rdf:type ?type.
+          ?attribute sio:has-value ?value.
+          ?compound sio:has-attribute ?attribute.
+          ?compound sio:has-attribute ?inchi.
+          ?inchi sio:has-value "{inchi}"@en.
+        }}
+        """
+        return await self.call_service(query)
+
+    async def call_service(self, query):
+        data = frozendict({"query": query})
+        response = await self.query_the_service('PubChem', '', method='POST', data=data, headers=self.header)
         if response:
             return self.parse_attributes(response)
 
@@ -63,16 +120,13 @@ class PubChem(Converter):
         :param response: given JSON
         :return: all parsed data
         """
-        response_json = json.loads(response)
+        response_json = eval(response)
         result = dict()
 
-        for prop in response_json['PC_Compounds'][0]['props']:
-            label = prop['urn']['label']
+        for prop in response_json['results']['bindings']:
+            identifier = prop['type']['value'].rsplit('/', 1)[-1]
+            value = prop['value']['value']
             for att in self.attributes:
-                if label == att['label']:
-                    if att['extra']:
-                        if prop['urn']['name'] == att['extra']:
-                            result[att['code']] = prop['value']['sval']
-                    else:
-                        result[att['code']] = prop['value']['sval']
+                if identifier == att['label']:
+                    result[att['code']] = value
         return result
