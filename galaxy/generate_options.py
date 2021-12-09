8c977fc2a05593e79cee368a53e3efd1d3d17cfd
Matej Trojak <trojak@mail.muni.cz>
2021-12-09 16:49:14 +0100

    Removed asyncio loop

--- galaxy/generate_options.py
+++ galaxy/generate_options.py
@@ -1,8 +1,7 @@
-import asyncio
 import sys
 import os
 
-# this add to path eBCSgen home dir, so it can be called from anywhere
+# this add to path the home dir, so it can be called from anywhere
 sys.path.append(os.path.split(sys.path[0])[0])
 
 from MSMetaEnhancer.libs.services import *
@@ -11,11 +10,8 @@ from MSMetaEnhancer.libs.services import __all__ as services
 
 def generate_options():
     jobs = []
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
     for service in services:
         jobs += (eval(service)(None).get_conversion_functions())
-    loop.close()
 
     for job in jobs:
         print(f'<option value="{job[0],job[1],job[2]}">{job[2]}: {job[0]} -> {job[1]}</option>')
