7c5d2221cbffca78d88eeff2c59235ea60509f70
xtrojak <trojak@mail.muni.cz>
2021-12-14 14:09:20 +0100

    Removed consistency tests for all services, only format checks are left

--- tests/test_CTS.py
+++ tests/test_CTS.py
@@ -3,45 +3,12 @@ import json
 import pytest
 
 from MSMetaEnhancer.libs.services.CTS import CTS
-from MSMetaEnhancer.libs.utils.Errors import UnknownResponse
 from tests.utils import wrap_with_session
 
 
 @pytest.mark.dependency()
 def test_service_available():
     asyncio.run(wrap_with_session(CTS, 'casno_to_inchikey', ['7783-89-3']))
-
-
-@pytest.mark.dependency(depends=["test_service_available"])
-@pytest.mark.parametrize('arg, value, expected, method', [
-    ['inchikey', '7783-89-3', 'XQLMNMQWVCXIKR-UHFFFAOYSA-M', 'casno_to_inchikey'],
-    ['inchi', 'XQLMNMQWVCXIKR-UHFFFAOYSA-M', 'InChI=1S/Ag.BrHO3/c;2-1(3)4/h;(H,2,3,4)/q+1;/p-1', 'inchikey_to_inchi'],
-    ['inchikey', 'L-Alanine', 'QNAYBMKLOCPYGJ-REOHCLBHSA-N', 'name_to_inchikey'],
-    ['name', 'QNAYBMKLOCPYGJ-REOHCLBHSA-N', 'L-ALANINE', 'inchikey_to_name'],
-    ['iupac_name', 'QNAYBMKLOCPYGJ-REOHCLBHSA-N', '(2S)-2-azaniumylpropanoate', 'inchikey_to_iupac_name']
-])
-def test_correct_behavior(arg, value, expected, method):
-    assert asyncio.run(wrap_with_session(CTS, method, [value]))[arg] == expected
-
-
-@pytest.mark.dependency(depends=["test_service_available"])
-@pytest.mark.parametrize('arg, value, method', [
-    ['inchi', 'XQLMNMQIKR-UHFFFAOYSA-M', 'inchikey_to_inchi'],
-    ['name', 'XQLMNMQIKR-UHFFFAOYSA-M', 'inchikey_to_name'],
-    ['iupac_name', 'XQLMNMQIKR-UHFFFAOYSA-M', 'inchikey_to_iupac_name']
-])
-def test_incorrect_behavior_exception(arg, value, method):
-    with pytest.raises(UnknownResponse):
-        asyncio.run(wrap_with_session(CTS, method, [value]))
-
-
-@pytest.mark.dependency(depends=["test_service_available"])
-@pytest.mark.parametrize('arg, value, method', [
-    ['inchikey', '7783893', 'casno_to_inchikey'],
-    ['inchikey', 'L-Alalalalanine', 'name_to_inchikey']
-])
-def test_incorrect_behavior_none(arg, value, method):
-    assert asyncio.run(wrap_with_session(CTS, method, [value])) is None
 
 
 @pytest.mark.dependency(depends=["test_service_available"])
--- tests/test_NLM.py
+++ tests/test_NLM.py
@@ -4,48 +4,12 @@ import pandas as pd
 import pytest
 
 from MSMetaEnhancer.libs.services.NLM import NLM
-from MSMetaEnhancer.libs.utils.Errors import UnknownResponse
 from tests.utils import wrap_with_session
 
 
 @pytest.mark.dependency()
 def test_service_available():
     asyncio.run(wrap_with_session(NLM, 'inchikey_to_name', ['QNAYBMKLOCPYGJ-REOHCLBHSA-N']))
-
-
-@pytest.mark.dependency(depends=["test_service_available"])
-@pytest.mark.parametrize('arg, value, expected, method', [
-    ['name', 'QNAYBMKLOCPYGJ-REOHCLBHSA-N', 'Alanine [USAN:INN]', 'inchikey_to_name'],
-    ['inchikey', 'L-Alanine', 'QNAYBMKLOCPYGJ-REOHCLBHSA-N', 'name_to_inchikey'],
-    ['formula', 'QNAYBMKLOCPYGJ-REOHCLBHSA-N', 'C3-H7-N-O2', 'inchikey_to_formula'],
-    ['casno', 'QNAYBMKLOCPYGJ-REOHCLBHSA-N', '56-41-7', 'inchikey_to_casno'],
-    ['formula', 'L-Alanine', 'C3-H7-N-O2', 'name_to_formula'],
-    ['casno', 'L-Alanine', '56-41-7', 'name_to_casno']
-])
-def test_correct_behavior(arg, value, expected, method):
-    assert asyncio.run(wrap_with_session(NLM, method, [value]))[arg] == expected
-
-
-@pytest.mark.dependency(depends=["test_service_available"])
-@pytest.mark.parametrize('arg, value, method', [
-    ['name', 'QNAYBMLOXXXXGJ-REOHCLBHSA-N', 'inchikey_to_name'],
-    ['inchikey', 'L-Alanne', 'name_to_inchikey'],
-    ['formula', 'L-Alanne', 'name_to_formula'],
-    ['casno', 'L-Alanne', 'name_to_casno']
-])
-def test_incorrect_behavior_exception(arg, value, method):
-    with pytest.raises(UnknownResponse):
-        asyncio.run(wrap_with_session(NLM, method, [value]))
-
-
-@pytest.mark.dependency(depends=["test_service_available"])
-@pytest.mark.parametrize('arg, value, method', [
-    ['name', 'QNAYMLGJ-REOLBHSA-N', 'inchikey_to_name'],
-    ['formula', 'QNAYMLGJ-REOLBHSA-N', 'inchikey_to_formula'],
-    ['casno', 'QNAYMLGJ-REOLBHSA-N', 'inchikey_to_casno']
-])
-def test_incorrect_behavior_none(arg, value, method):
-    assert asyncio.run(wrap_with_session(NLM, method, [value])) is None
 
 
 @pytest.mark.dependency(depends=["test_service_available"])
