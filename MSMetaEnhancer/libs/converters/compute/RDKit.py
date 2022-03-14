70dddf34ab555d262808e77cf81b8d583faa310f
Matej Trojak <trojak@mail.muni.cz>
2022-03-14 14:36:22 +0100

    Fixed typos in RDKit

--- MSMetaEnhancer/libs/converters/compute/RDKit.py
+++ MSMetaEnhancer/libs/converters/compute/RDKit.py
@@ -5,14 +5,18 @@ from MSMetaEnhancer.libs.converters.compute.ComputeConverter import ComputeConve
 
 
 class RDKit(ComputeConverter):
+    """
+    RDKit is a collection of cheminformatics and machine-learning software.
+    """
     def __init__(self):
         super().__init__()
         # generate top level methods defining allowed conversions
         conversions = [('smiles', 'mw', 'from_smiles'),
                        ('canonical_smiles', 'mw', 'from_smiles'),
                        ('isomeric_smiles', 'mw', 'from_smiles')]
-        self.create_top_level_conversion_methods(conversions)
+        self.create_top_level_conversion_methods(conversions, asynch=False)
 
     def from_smiles(self, smiles):
         weight = ExactMolWt(MolFromSmiles(smiles))
-        return {'hw': weight}
+        return {'mw': weight}
+    
