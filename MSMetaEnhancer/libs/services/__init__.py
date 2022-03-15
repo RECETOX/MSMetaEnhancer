e48049b19ada63da1b5fcf55bf871ae6945e291c
Matej Trojak <trojak@mail.muni.cz>
2022-03-15 17:07:46 +0100

    Added BridgeDB converter supporting several DB IDs

--- MSMetaEnhancer/libs/services/__init__.py
+++ MSMetaEnhancer/libs/services/__init__.py
@@ -3,5 +3,6 @@ from MSMetaEnhancer.libs.services.CTS import CTS
 from MSMetaEnhancer.libs.services.NLM import NLM
 from MSMetaEnhancer.libs.services.IDSM import IDSM
 from MSMetaEnhancer.libs.services.PubChem import PubChem
+from MSMetaEnhancer.libs.services.BridgeDB import BridgeDB
 
-__all__ = ['IDSM', 'CTS', 'CIR', 'NLM', 'PubChem']
+__all__ = ['IDSM', 'CTS', 'CIR', 'NLM', 'PubChem', 'BridgeDB']
