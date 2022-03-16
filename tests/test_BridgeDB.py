c8c224b8d44990c700fe22e42527de8b2017a3c6
xtrojak <trojak@mail.muni.cz>
2022-03-16 13:04:36 +0100

    Added simple tests for BridgeDB

---
+++ tests/test_BridgeDB.py
@@ -0,0 +1,29 @@
+import asyncio
+import pytest
+
+from MSMetaEnhancer.libs.converters.web import BridgeDB
+from tests.utils import wrap_with_session
+
+
+HMDBID = 'HMDB0000001'
+
+
+@pytest.mark.dependency()
+def test_service_available():
+    asyncio.run(wrap_with_session(BridgeDB, 'hmdbid_to_pubchemid', ['HMDB0000001']))
+
+
+@pytest.mark.dependency(depends=["test_service_available"])
+def test_format():
+    args = f'Ch/{HMDBID}'
+    response = asyncio.run(wrap_with_session(BridgeDB, 'query_the_service', ['BridgeDB', args]))
+
+    assert type(response) == str
+    lines = response.split('\n')
+    assert len(lines) != 0
+    assert '\t' in response
+
+
+def test_get_conversions():
+    jobs = BridgeDB(None).get_conversion_functions()
+    assert ('wikidataid', 'pubchemid', 'BridgeDB') in jobs
