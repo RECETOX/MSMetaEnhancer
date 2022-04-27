1e9a11fa8555dfb2676874d14b61b6d0a30e9bbe
hechth <helge.hecht@recetox.muni.cz>
2022-04-27 16:55:08 +0200

    Updated long description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
 setup(name='MSMetaEnhancer',
       version='0.2.1',
       description='Repository for tool that adds more annotations '
                   '(e.g. SMILES, InChI, CAS number) to MSP files (Python version).',
+      long_description=readme,
+      long_description_content_type="text/markdown",
       url='https://github.com/RECETOX/MSMetaEnhancer',
       author='Matej Trojak',
       author_email='matej.trojak@recetox.muni.cz',
