1950ebf67d64d441d4898ed1a81d8e73e9396bd2
hechth <helge.hecht@recetox.muni.cz>
2022-04-27 16:36:17 +0200

    lint

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,6 @@
 import asyncio
 import sys
 
-
 if sys.platform == 'win32':
     # Set the policy to prevent "Event loop is closed" error on Windows - https://github.com/encode/httpx/issues/914
     # See https://stackoverflow.com/questions/63860576/asyncio-event-loop-is-closed-when-using-asyncio-run
