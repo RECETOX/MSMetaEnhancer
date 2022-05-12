a516dbb58335a50fb75cbeca18e2690aec5e2b5b
Matej Trojak <trojak@mail.muni.cz>
2022-05-12 11:09:05 +0200

    Introduced KEGG ID conversions to BridgeDB service

--- MSMetaEnhancer/libs/converters/web/BridgeDB.py
+++ MSMetaEnhancer/libs/converters/web/BridgeDB.py
@@ -13,31 +13,42 @@ class BridgeDB(WebConverter):
         # service URLs
         self.endpoints = {'BridgeDB': 'https://webservice.bridgedb.org/Human/xrefs/'}
 
-        self.codes = {'hmdbid': 'Ch', 'pubchemid': 'Cpc', 'chemspiderid': 'Cs', 'wikidataid': 'Wd', 'chebiid': 'Ce'}
+        self.codes = {'hmdbid': 'Ch', 'pubchemid': 'Cpc', 'chemspiderid': 'Cs', 'wikidataid': 'Wd', 'chebiid': 'Ce',
+                      'keggid': 'Ck'}
         self.identifiers = {'PubChem-compound': 'pubchemid', 'Chemspider': 'chemspiderid', 'ChEBI': 'chebiid',
-                            'HMDB': 'hmdbid', 'Wikidata': 'wikidataid'}
+                            'HMDB': 'hmdbid', 'Wikidata': 'wikidataid', 'KEGG Compound': 'keggid'}
 
         # generate top level methods defining allowed conversions
         conversions = [('hmdbid', 'pubchemid', 'from_hmdbid'),
                        ('hmdbid', 'chemspiderid', 'from_hmdbid'),
                        ('hmdbid', 'wikidataid', 'from_hmdbid'),
                        ('hmdbid', 'chebiid', 'from_hmdbid'),
+                       ('hmdbid', 'keggid', 'from_hmdbid'),
                        ('pubchemid', 'hmdbid', 'from_pubchemid'),
                        ('pubchemid', 'chemspiderid', 'from_pubchemid'),
                        ('pubchemid', 'wikidataid', 'from_pubchemid'),
                        ('pubchemid', 'chebiid', 'from_pubchemid'),
+                       ('pubchemid', 'keggid', 'from_pubchemid'),
                        ('chemspiderid', 'hmdbid', 'from_chemspiderid'),
                        ('chemspiderid', 'pubchemid', 'from_chemspiderid'),
                        ('chemspiderid', 'wikidataid', 'from_chemspiderid'),
                        ('chemspiderid', 'chebiid', 'from_chemspiderid'),
+                       ('chemspiderid', 'keggid', 'from_chemspiderid'),
                        ('wikidataid', 'hmdbid', 'from_wikidataid'),
                        ('wikidataid', 'pubchemid', 'from_wikidataid'),
                        ('wikidataid', 'chemspiderid', 'from_wikidataid'),
                        ('wikidataid', 'chebiid', 'from_wikidataid'),
+                       ('wikidataid', 'keggid', 'from_wikidataid'),
                        ('chebiid', 'hmdbid', 'from_chebiid'),
                        ('chebiid', 'pubchemid', 'from_chebiid'),
                        ('chebiid', 'chemspiderid', 'from_chebiid'),
-                       ('chebiid', 'wikidataid', 'from_chebiid')
+                       ('chebiid', 'wikidataid', 'from_chebiid'),
+                       ('chebiid', 'keggid', 'from_chebiid'),
+                       ('keggid', 'hmdbid', 'from_keggid'),
+                       ('keggid', 'pubchemid', 'from_keggid'),
+                       ('keggid', 'chemspiderid', 'from_keggid'),
+                       ('keggid', 'wikidataid', 'from_keggid'),
+                       ('keggid', 'chebiid', 'from_keggid'),
                        ]
         self.create_top_level_conversion_methods(conversions)
 
@@ -89,6 +100,16 @@ class BridgeDB(WebConverter):
         :return: obtained IDs
         """
         args = f'{self.codes["chebiid"]}/{chebiid}'
+        return await self.call_service(args)
+
+    async def from_keggid(self, keggid):
+        """
+        Convert KEGG ID to all possible IDs using BridgeDB web service
+
+        :param keggid: given KEGG ID number
+        :return: obtained IDs
+        """
+        args = f'{self.codes["keggid"]}/{keggid}'
         return await self.call_service(args)
 
     async def call_service(self, args):
