69c65a98faa9d2790dc53be29cdf4cb5fa6598f5
Matej Trojak <trojak@mail.muni.cz>
2023-04-05 14:20:36 +0200

    Added RDKit tests

---
+++ tests/test_rdkit.py
@@ -0,0 +1,18 @@
+import asyncio
+import pytest
+
+from MSMetaEnhancer.libs.converters.compute import RDKit
+
+
+INCHI = 'InChI=1S/C19H28O2/c1-18-9-7-13(20)11-12(18)3-4-14-15-5-6-17(21)19(15,2)10-8-16(14)18/h11,14-17,21H,3-10H2,1-2H3/t14-,15-,16-,17-,18-,19-/m0/s1'
+CANONICAL_SMILES = 'CC12CCC(=O)C=C1CCC1C2CCC2(C)C(O)CCC12'
+
+@pytest.mark.parametrize('method, input, expected', [
+    ['inchi_to_canonical_smiles', INCHI, {'canonical_smiles': CANONICAL_SMILES}],
+    ['inchi_to_isomeric_smiles', INCHI, {'isomeric_smiles': 'C[C@]12CC[C@H]3[C@@H](CCC4=CC(=O)CC[C@@]43C)[C@@H]1CC[C@@H]2O'}],
+    ['from_smiles', CANONICAL_SMILES, {'mw': 288.208930136}]
+])
+def test_convert_methods(method, input, expected):
+    func = getattr(RDKit(), method)  
+    actual = func(input)
+    assert actual == expected
