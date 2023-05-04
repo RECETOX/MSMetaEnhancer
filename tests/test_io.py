075aa64ec17c6bc7c0e07de6097a6d354e459ecc
Matej Trojak <trojak@mail.muni.cz>
2023-05-04 09:11:37 +0200

    Add tests for IO

---
+++ tests/test_io.py
@@ -0,0 +1,37 @@
+import pytest
+import pandas
+import mock
+
+from MSMetaEnhancer.libs.data import Spectra, DataFrame
+
+
+DATA = [{'formula': 'H2', 'mw': '2', 'casno': '1333740', 'id': '1', 'num_peaks': '2', 'compound_name': 'Hydrogen'},
+        {'formula': 'D2', 'mw': '4', 'casno': '7782390', 'id': '2', 'num_peaks': '2', 'compound_name': 'Deuterium'},
+        {'formula': 'CH4', 'mw': '16', 'casno': '74828', 'id': '3', 'num_peaks': '6', 'compound_name': 'Methane'}]
+
+
+@pytest.mark.parametrize('backend, file_type, filename', [
+    [Spectra(), 'msp', 'tests/test_data/sample.msp'],
+    [DataFrame(), 'csv', 'tests/test_data/sample_metadata.csv']
+])
+def test_get_metadata(backend, file_type, filename):
+    getattr(backend, f'load_from_{file_type}')(filename)
+    assert backend.get_metadata() == DATA
+
+
+def test_fuse_metadata_dataframe():
+    pandas_df = pandas.read_csv('tests/test_data/sample_metadata.csv', dtype=str)
+    df = DataFrame()
+    df.fuse_metadata(DATA)
+    assert pandas_df.equals(df.df)
+
+
+def test_fuse_metadata_spectra():
+    spectra_fused = Spectra()
+    spectra_fused.spectrums = [mock.Mock(metadata=dict()), mock.Mock(metadata=dict()), mock.Mock(metadata=dict())]
+    spectra_fused.fuse_metadata(DATA)
+
+    spectra_loaded = Spectra()
+    spectra_loaded.load_from_msp('tests/test_data/sample.msp')
+
+    assert spectra_fused.get_metadata() == spectra_loaded.get_metadata()
