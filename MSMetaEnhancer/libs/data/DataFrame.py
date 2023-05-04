dc0abc73401e5413f337889f8c6cd2233e79edb2
Matej Trojak <trojak@mail.muni.cz>
2023-05-04 09:54:06 +0200

    Improve DataFrame docs

--- MSMetaEnhancer/libs/data/DataFrame.py
+++ MSMetaEnhancer/libs/data/DataFrame.py
@@ -8,9 +8,19 @@ class DataFrame(Data):
         self.df = pandas.DataFrame()
 
     def load_from_csv(self, filename):
+        """
+        Loads metadata from CSV file and stores them in pandas DataFrame.
+
+        :param filename: given CSV file
+        """
         self.df = pandas.read_csv(filename, dtype=str)
 
     def save_to_csv(self, filename):
+        """
+        Store metadata as a table to given CSV file.
+
+        :param filename: target CSV file
+        """
         self.df.to_csv(filename, index=False)
 
     def get_metadata(self):
