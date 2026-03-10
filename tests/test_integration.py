a1088d7c7434411b21acd661efd26d7e6751db30
hechth <helge.hecht@recetox.muni.cz>
2026-03-10 11:13:48 +0100

    added integration test and updated docs

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -8,28 +8,34 @@ from MSMetaEnhancer.libs.utils.ConverterBuilder import ConverterBuilder
 
 ConverterBuilder.register([CTS, CIR, IDSM, PubChem, BridgeDb, RDKit])
 
+
 def test_integration(tmp_path):
     app = Application()
 
     # import your .msp file
-    app.load_data('tests/test_data/sample.msp', file_format='msp')
+    app.load_data("tests/test_data/sample.msp", file_format="msp")
 
     # curate given metadata (e.g. fix CAS numbers)
     app.curate_metadata()
 
     # specify requested services (these are supported)
-    services = ['CTS', 'CIR', 'IDSM', 'PubChem', 'BridgeDb', 'RDKit']
+    services = ["CTS", "CIR", "IDSM", "PubChem", "BridgeDb", "RDKit"]
 
     # specify requested jobs
-    jobs = [('name', 'inchi', 'IDSM'), ('inchi', 'formula', 'IDSM'), ('inchi', 'inchikey', 'IDSM'),
-            ('inchi', 'iupac_name', 'IDSM'), ('inchi', 'canonical_smiles', 'IDSM')]
+    jobs = [
+        ("compound_name", "inchi", "IDSM"),
+        ("inchi", "formula", "IDSM"),
+        ("inchi", "inchikey", "IDSM"),
+        ("inchi", "iupac_name", "IDSM"),
+        ("inchi", "canonical_smiles", "IDSM"),
+    ]
 
     # run asynchronous annotations of spectra data
     asyncio.run(app.annotate_spectra(services, jobs))
 
     # export .msp file
-    outpath = os.path.join(tmp_path, 'sample_out.msp')
-    app.save_data(outpath, file_format='msp')
+    outpath = os.path.join(tmp_path, "sample_out.msp")
+    app.save_data(outpath, file_format="msp")
     assert os.path.isfile(outpath)
 
     os.remove(outpath)
